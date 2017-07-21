f353c30a7d1d8fb6bf8df0102ad099f07c7be3db
Liam Prendergast <Liam@Liams-MacBook-Pro-2.local>
2017-07-21 09:35:03 -0400

    Updated and tested methods

--- src/main/scala/HexameterParser.scala
+++ src/main/scala/HexameterParser.scala
@@ -13,4 +13,23 @@ object HexameterParser {
     punctuation.replaceAllIn(noAcc, "")
   }
 
+ def isolateDiaresis(s: String) : String = {
+   val diaresis = """([hweoaiu])(\+)""".r
+   diaresis.replaceAllIn(s, " $1 ")
+ }
+
+ def accountForCorreption(s: String) : String = {
+   val epicCorreption = """(ai|au|oi|ou|ui|ei|eu|[hw]) *([hwaeiou])""".r
+   epicCorreption.replaceAllIn(s, "? $2")
+ }
+
+ def longByPosition(s: String) : String = {
+   val longPosition= """(ai|au|ou|oi|ui|ei|eu|[hweoaiu])( ?)((zyc)|([qrtpsdygklzxcbnmf]( ?)[qrtpsdygklzxcbnmf]))""".r
+   longPosition.replaceAllIn(n, "-")
+ }
+
+ def longByNature(s: String) : String = {
+  val longNature= """ai|au|oi|ui|ei|eu|ou|[hw]""".r
+  longNature.replaceAllIn(s,"-")
+ }
 }
--- src/test/scala/HexameterParserSpec.scala
+++ src/test/scala/HexameterParserSpec.scala
@@ -6,13 +6,27 @@ import edu.holycross.shot.greek._
 
 
 class HexameterParserSpec extends FlatSpec {
-
+/*
   "A HexameterParser"  should  "strip punctuation from a Greek string" in {
     val gs = LiteraryGreekString("ἄειδε,")
     val stripped = HexameterParser.stripNonAlphabetic(gs)
     val expected = "aeide"
     assert (stripped == expected)
   }
+
+  "A HexameterParser" should "Isolate vowels that have a diaresis" in {
+    val s = "eyxei+"
+    val isolated = HexameterParser.isolateDiaresis(s)
+    val exp = "eyxe i "
+    assert (isolated == exp)
+  }
+*/
+ "A HexameterParser" should "replace uncertain vowels due to epic correption with ?" in {
+   val n = "ei o"
+   val correpted = HexameterParser.accountForCorreption(n)
+   val exp = "? o"
+   assert (correpted == exp)
+ }
 
 
 }
