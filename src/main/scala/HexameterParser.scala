ccdf4d1e8333ba1faeb3b491f96a4db13fc53504
Liam Prendergast <Liam@Liams-MacBook-Pro-2.local>
2017-07-22 15:20:42 -0400

    Updated and tested methods

--- src/main/scala/HexameterParser.scala
+++ src/main/scala/HexameterParser.scala
@@ -7,29 +7,71 @@ import edu.holycross.shot.gsphone._
 
 object HexameterParser {
 
+/** Returns string of ascii characters without accents, breathings, or punctuation
+ *  takes LiteraryGreekString
+ *  only keeps diareses and letters
+ * @param gs the LiteraryGreekString
+*/
   def stripNonAlphabetic(gs: GreekString) : String = {
     val noAcc = gs.stripAccent.ascii
     val punctuation= """[\(\)|,;:.'"]""".r
     punctuation.replaceAllIn(noAcc, "")
   }
 
+/** Returns string with isolated vowels that contain diareses
+ *  takes a String of ascii characters
+ *  @param s the String of ascii characters
+*/
  def isolateDiaresis(s: String) : String = {
    val diaresis = """([hweoaiu])(\+)""".r
    diaresis.replaceAllIn(s, " $1 ")
  }
 
+/** Returns string with vowels or dipthongs that might undergo epic correption marked as "?"
+ *  takes a String of ascii characters
+ *  accounts for internal epic correption as well
+ *  @param s the String
+*/
  def accountForCorreption(s: String) : String = {
    val epicCorreption = """(ai|au|oi|ou|ui|ei|eu|[hw]) *([hwaeiou])""".r
    epicCorreption.replaceAllIn(s, "? $2")
  }
 
+ /** Returns string with vowels or dipthongs that are long by position marked as "-"
+  *  takes a String of ascii characters
+  *  accounts for a vowel or dipthong followed by two consonants or a double consonant
+  *  @param s the String
+ */
  def longByPosition(s: String) : String = {
    val longPosition= """(ai|au|ou|oi|ui|ei|eu|[hweoaiu])( ?)((zyc)|([qrtpsdygklzxcbnmf]( ?)[qrtpsdygklzxcbnmf]))""".r
-   longPosition.replaceAllIn(n, "-")
+   longPosition.replaceAllIn(s, "-$2$3")
  }
 
+ /** Returns String with vowels or dipthongs that are long by nature marked as "-"
+  *  takes a String of ascii characters
+  *  @param s the String
+ */
  def longByNature(s: String) : String = {
   val longNature= """ai|au|oi|ui|ei|eu|ou|[hw]""".r
   longNature.replaceAllIn(s,"-")
  }
+
+ /** Returns String without consonants or spaces, leaves only vowels and lengths ("?" or "-")
+  *  @param s the String
+ */
+ def removeConsonants(s: String) : String = {
+  val justVowels = """[^hweoiua?-]""".r
+  justVowels.replaceAllIn(s,"")
+ }
+
+ /** Returns String that replaces all characters that aren't lengths ("?" or "-") with "?"
+  *  @param s the String
+ */
+ def makeLengths(s: String) : String = {
+  val notLengths = """[^?-]""".r
+  notLengths.replaceAllIn(s,"?")
+ }
+ 
+
+
 }
--- src/test/scala/HexameterParserSpec.scala
+++ src/test/scala/HexameterParserSpec.scala
@@ -6,7 +6,7 @@ import edu.holycross.shot.greek._
 
 
 class HexameterParserSpec extends FlatSpec {
-/*
+
   "A HexameterParser"  should  "strip punctuation from a Greek string" in {
     val gs = LiteraryGreekString("ἄειδε,")
     val stripped = HexameterParser.stripNonAlphabetic(gs)
@@ -14,19 +14,45 @@ class HexameterParserSpec extends FlatSpec {
     assert (stripped == expected)
   }
 
-  "A HexameterParser" should "Isolate vowels that have a diaresis" in {
+  "A HexameterParser" should "isolate vowels that have a diaresis" in {
     val s = "eyxei+"
     val isolated = HexameterParser.isolateDiaresis(s)
     val exp = "eyxe i "
     assert (isolated == exp)
   }
-*/
+
  "A HexameterParser" should "replace uncertain vowels due to epic correption with ?" in {
-   val n = "ei o"
-   val correpted = HexameterParser.accountForCorreption(n)
-   val exp = "? o"
-   assert (correpted == exp)
+   val s = "ei o"
+   val correpted = HexameterParser.accountForCorreption(s)
+   val expected = "? o"
+   assert (correpted == expected)
  }
 
+ "A HexameterParser" should "replace vowels/dipthongs that are long by position with -" in {
+   val s = "pem pwmenon"
+   val longPosition = HexameterParser.longByPosition(s)
+   val expected = "p-m pwmenon"
+   assert (longPosition == expected)
+ }
 
+ "A HexameterParser" should "replace vowels/dipthongs that are long by nature with -" in {
+   val s = "wka xamai"
+   val longNature = HexameterParser.longByNature(s)
+   val expected = "-ka xam-"
+   assert (longNature == expected)
+ }
+
+ "A HexameterParser" should "remove consonants and spaces from a string, leaving only vowels and lengths" in {
+   val s = "p-m p-menon"
+   val noConsonants = HexameterParser.removeConsonants(s)
+   val expected = "--eo"
+   assert (noConsonants == expected)
+ }
+
+ "A HexameterParser" should "replace all characters that are not a length with '?'" in {
+   val s = "--eo"
+   val lengths = HexameterParser.makeLengths(s)
+   val expected = "--??"
+   assert (lengths == expected)
+ }
 }
